75c3402cb588ec592354edf4c4cd81b0258db469
ambas <ambaschobsanti@gmail.com>
2015-02-06 00:25:10 +0700

    made demo project and change class name

---
+++ BifrostController/BifrostController/BifrostController.h
@@ -0,0 +1,19 @@
+//
+//  BifrostController.h
+//  BifrostController
+//
+//  Created by Ambas Chobsanti on 2/6/2558 BE.
+//  Copyright (c) 2558 TW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BifrostController.
+FOUNDATION_EXPORT double BifrostControllerVersionNumber;
+
+//! Project version string for BifrostController.
+FOUNDATION_EXPORT const unsigned char BifrostControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BifrostController/PublicHeader.h>
+
+
