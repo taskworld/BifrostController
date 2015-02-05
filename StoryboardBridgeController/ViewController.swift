0f1c3ad48348dee3c960ac55425c82cc42b1e984
ambas <ambas.c@taskworld.com>
2015-02-05 17:53:16 +0700

    add all stuff

---
+++ StoryboardBridgeController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoryboardBridgeController
+//
+//  Created by Ambas Chobsanti on 2/4/15.
+//  Copyright (c) 2015 Taskworld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
